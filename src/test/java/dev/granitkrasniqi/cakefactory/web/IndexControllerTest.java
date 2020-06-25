b5ff240f16da8ae2c47c363a29978bb136e8ce64
Granit Krasniqi <granit.krasniqi@perpetuum.eu>
2020-06-25 20:31:24 +0200

    ADDED IndexControllerTest

---
+++ src/test/java/dev/granitkrasniqi/cakefactory/web/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package dev.granitkrasniqi.cakefactory.web;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IndexControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("index page returns the landing page")
+    void returnsLandingPage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Cake Factory")));
+    }
+}
